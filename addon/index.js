bdf57ca63e62dfd844bb3c04ae43755de491f2f5
Sergey Astapov <sergeastapov@gmail.com>
2021-11-14 19:42:41 -0500

    Fix re-exports in Ember >= v4.0.0-beta.9

--- addon/index.js
+++ addon/index.js
@@ -1,27 +1,39 @@
 /* eslint-disable ember/new-module-imports */
 import Ember from 'ember';
-import { macroCondition, dependencySatisfies } from '@embroider/macros';
+import {
+  macroCondition,
+  dependencySatisfies,
+  importSync,
+} from '@embroider/macros';
 
 export const Checkbox = macroCondition(
   dependencySatisfies('ember-source', '>= 3.27.0-beta.1')
 )
-  ? Ember._LegacyCheckbox.extend()
+  ? macroCondition(dependencySatisfies('ember-source', '< v4.0.0-beta.9'))
+    ? Ember._LegacyCheckbox.extend()
+    : importSync('@ember/legacy-built-in-components/components/checkbox')
   : Ember.Checkbox;
 
 export const TextField = macroCondition(
   dependencySatisfies('ember-source', '>= 3.27.0-beta.1')
 )
-  ? Ember._LegacyTextField.extend()
+  ? macroCondition(dependencySatisfies('ember-source', '< v4.0.0-beta.9'))
+    ? Ember._LegacyTextField.extend()
+    : importSync('@ember/legacy-built-in-components/components/text-field')
   : Ember.TextField;
 
 export const TextArea = macroCondition(
   dependencySatisfies('ember-source', '>= 3.27.0-beta.1')
 )
-  ? Ember._LegacyTextArea.extend()
+  ? macroCondition(dependencySatisfies('ember-source', '< v4.0.0-beta.9'))
+    ? Ember._LegacyTextArea.extend()
+    : importSync('@ember/legacy-built-in-components/components/textarea')
   : Ember.TextArea;
 
 export const LinkComponent = macroCondition(
   dependencySatisfies('ember-source', '>= 3.27.0-beta.1')
 )
-  ? Ember._LegacyLinkComponent.extend()
+  ? macroCondition(dependencySatisfies('ember-source', '< v4.0.0-beta.9'))
+    ? Ember._LegacyLinkComponent.extend()
+    : importSync('@ember/legacy-built-in-components/components/link-to')
   : Ember.LinkComponent;
